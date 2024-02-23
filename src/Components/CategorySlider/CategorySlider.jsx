572b0626ecb0fe8e39537cc2c58f48671ce4a265
kareem-gafer10 <kareemgafer28.com>
2024-02-23 16:01:08 +0200

    fix responsive category slider

--- src/Components/CategorySlider/CategorySlider.jsx
+++ src/Components/CategorySlider/CategorySlider.jsx
@@ -2,12 +2,9 @@ import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import { Link } from "react-router-dom";
 import baseInstance from "../../Networking/baseInstance";
-import "./CategorySlider.css"
-
-
+import "./CategorySlider.css";
 
 const CategorySlider = () => {
-  
   const [categories, setCategories] = useState([]);
 
   const getCategories = async () => {
@@ -33,7 +30,7 @@ const CategorySlider = () => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll:2,
+          slidesToScroll: 2,
         },
       },
       {
@@ -46,7 +43,7 @@ const CategorySlider = () => {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll:2,
+          slidesToScroll: 2,
         },
       },
     ],
@@ -56,29 +53,30 @@ const CategorySlider = () => {
     const { _id, image, name } = category;
     return (
       <div key={_id} className="sliderItem">
-      <div>
-        <img height={300} src={image} alt={name} />
+        <div>
+          <img height={300} src={image} alt={name} />
+        </div>
+        <h3 className="text-center h6 mt-3 text-main">{name}</h3>
       </div>
-      <h3 className="text-center h6 mt-3 text-main">{name}</h3>
-    </div>
     );
   });
 
   return (
     <>
-    <div className="text-category d-flex justify-content-between align-items-center">
-    <div>
-    <h3 className='py-4 text-main'>Shop Categories:</h3>
-    </div>
+      <div className="text-category d-flex justify-content-between align-items-center">
+        <div>
+          <h3 className="py-4 text-main ms-3 fw-bold">Shop Categories:</h3>
+        </div>
 
-    <h3>
-    <Link className=" text-main"  to="categories">See All </Link>
-    </h3>
-    
-    </div>
+        <h3>
+          <Link className=" text-main me-3" to="categories">
+            See All{" "}
+          </Link>
+        </h3>
+      </div>
       <Slider {...settings}>{allCategories}</Slider>
     </>
   );
-}
+};
 
 export default CategorySlider;
